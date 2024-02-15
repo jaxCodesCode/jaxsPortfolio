6a82b292cc03b419c8aeeea77fb45ca65c177955
jaxCodesCode <143358866+jaxCodesCode@users.noreply.github.com>
2024-02-15 16:22:37 -0700

    update resume and animations

--- src/app/screens/landing-screen/landing-screen.component.ts
+++ src/app/screens/landing-screen/landing-screen.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -7,7 +7,6 @@ import { ScrollSnapService } from 'src/app/services/scroll-snap/scroll-snap.serv
 import { SocialLinkComponent } from 'src/app/components/social-link/social-link.component';
 import { UtilService } from 'src/app/services/util/util.service';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
-import { MobileNoticeDialogComponent } from 'src/app/components/mobile-notice-dialog/mobile-notice-dialog.component';
 import { AnimateOnScrollModule } from 'primeng/animateonscroll';
 import { AnimateModule } from 'primeng/animate'
 
@@ -18,7 +17,7 @@ import { AnimateModule } from 'primeng/animate'
   templateUrl: './landing-screen.component.html',
   styleUrls: ['./landing-screen.component.scss']
 })
-export class LandingScreenComponent implements OnInit {
+export class LandingScreenComponent {
 
   constructor(private readonly scrollSnapService: ScrollSnapService,
             private readonly utilService: UtilService,
@@ -26,12 +25,6 @@ export class LandingScreenComponent implements OnInit {
   {
 
   }
-
- ngOnInit(): void {
-   if (this.utilService.deviceIsMobile()) {
-     this.dialog.open(MobileNoticeDialogComponent)
-   } 
- }
 
   enterPortfolio = () => {
     this.scrollSnapService.scrollToElement('about');
